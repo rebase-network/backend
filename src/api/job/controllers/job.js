c967df01d965d4f644eb92e1f6e01f28f739ce45
liushooter <shooter.hero@gmail.com>
2022-08-14 00:07:22 +0800

    github issue api

--- src/api/job/controllers/job.js
+++ src/api/job/controllers/job.js
@@ -3,7 +3,26 @@
 /**
  *  job controller
  */
+const axios = require('axios');
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::job.job');
+module.exports = createCoreController('api::job.job', ({ strapi }) => ({
+
+  async find(ctx) {
+    const url = "https://api.github.com/repos/rebase-network/who-is-hiring/issues?per_page=5&page=1"
+
+    const resp = await axios.get(url);
+    console.log("resp.status" + resp.status)
+    const jobs = await resp.data;
+
+    // some custom logic here
+    ctx.query = { ...ctx.query, local: 'en' }
+
+    // Calling the default core action
+    const { data, meta } = await super.find(ctx);
+
+    return { data, jobs, meta };
+  },
+
+}));
